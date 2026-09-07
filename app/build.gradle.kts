cea7fe9a274fe62ce4e7f1a5412e2217f3622057
zayar151183-star <zayar151183@gmail.com>
2026-09-07 20:51:11 +0630

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.zayar.tempbox"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.zayar.tempbox"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
