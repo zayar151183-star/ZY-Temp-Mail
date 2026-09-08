0365e303e82266813b2b9a38e7c98bb17665545f
zayar151183-star <zayar151183@gmail.com>
2026-09-08 11:52:50 +0630

    Make email buttons and links clickable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.zayar.tempbox"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
     }
 }
